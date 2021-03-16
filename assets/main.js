092e7c625cc750b40670842206e41822dd4f1ff4
Erickson Powell <ericksonpowell@gmail.com>
2021-03-16 11:55:39 -0700

    working on 'render' loop for 5day forcast

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,7 @@
 // u06-weather-dashboard main.js created by Erickson Powell for UW-SEA-FSF Coding Bootcamp
 
 //API WIZARDRY
-const APIKey = "a3b4f18c52ad53eb1775853a7b8fe0c0";
-const APIKeyName = "homework";
+const API_KEY = "a3b4f18c52ad53eb1775853a7b8fe0c0";
 
 //get previously searched cities, or empty array (fresh localstorage/search)
 let storedCities = JSON.parse(localStorage.getItem("storedCities")) || [];
@@ -16,26 +15,55 @@ let cityName, cityTemp, cityHumidity, windSpeed, uvIndex;
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 
 // API CALLLLLLLLL
-function getWeatherByCity() {
+function getWeatherByCity(cityToSearch) {
   // let city = "seattle"     //test line
   //first call is to grab lat&lon
-  let cityQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${APIKey}`
+  let cityQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${API_KEY}`
 
   //API
   $.ajax({
     url: cityQueryURL,
     method: "GET"
   }).then(function(cityLatLonRes) {
+    //log the response
     console.log(cityLatLonRes);
+    //set lat/lon for detailed api call
     let lat = cityLatLonRes.coord.lat;
     let lon = cityLatLonRes.coord.lon;
     //second call is to get all weather data needed for application
-    let weatherQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${APIKey}`
+    let weatherQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${API_KEY}`
     $.ajax({
       url: weatherQueryURL,
       method: "GET"
     }).then(function(weatherRes) {
+      //log response
       console.log(weatherRes);
+      //this is where i will fill vars from response
+      let ccTemp = parseFloat(((weatherRes.current.temp) - 273.15) * (9/5) + 32).toPrecision(4) + "°F";
+      let ccHumid = (weatherRes.current.humidity) + "%";
+      let ccWindSpeed = weatherRes.current.wind_speed;
+      let ccUVIndex = weatherRes.current.uvi;
+      let ccDate = moment().format("dddd, MMMM Do YYYY");
+      console.log(`Temp: ${ccTemp}, Humidity: ${ccHumid}, Wind Speed: ${ccWindSpeed}, UV Index: ${ccUVIndex}`);
+      
+      $("#ccHeader").text(cityToSearch);
+      $("#ccDate").text(ccDate);
+      $("#ccTemp").text(`Temp: ${ccTemp}`);
+      $("#ccHumid").text(`Humidity: ${ccHumid}`);
+      $("#ccWindSpeed").text(`Wind Speed: ${ccWindSpeed}`);
+      $("#ccUVIndex").text(`UV Index: ${ccUVIndex}`);
+
+      //5day loop start
+      let fiveDayForcast = weatherRes.daily;
+
+      for(let i = 1; i <= 5; i++) {
+        $(`#day${i}date`).text(moment().add(i, "days").format("MM/DD/YY"));
+        $(`#day${i}temp`).text(`${daily[i].temp.day}`);
+        $(`#day${i}humid`).text(`${i}`);
+        $(`#day${i}icon`).text(`${i}`);  
+      }
+
+
     }).catch((error) => console.log("Second API call error: ", error))
     
   }).catch((error) => console.log("First API call error: ", error))
@@ -49,13 +77,17 @@ $("#searchCity").click(function(e) {
   cityToSearch = $(this).parent().find("input").val();
   console.log(cityToSearch);
   //passes city into API call function
-  getWeatherByCity(cityToSearch);
+  detailedSearchRes = getWeatherByCity(cityToSearch);
+  
+  displayCurrentSearch(detailedSearchRes);
+  
 });
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
-
+function displayCurrentSearch(detailedSearchRes) {
+}
 
 
 
